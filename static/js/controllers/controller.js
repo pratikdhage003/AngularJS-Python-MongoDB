8bc41aa7bae0a5845e83bb2995b49ff4b880f362
Pratik Dhage <pratikdhage@Pratiks-MacBook-Pro.local>
2017-07-27 17:33:57 -0700

    edited controller.js

--- static/js/controllers/controller.js
+++ static/js/controllers/controller.js
@@ -11,73 +11,35 @@ app.config(['$interpolateProvider', function ($interpolateProvider) {
 app.controller("AppCtrl", function ($scope, $http) {
     var app = this;
 
-    app.message = "let's solve it .....";
-
     var refreshData = function () {
-
         $http.get('http://0.0.0.0:8000/todo/api/v1.0/cities')
             .then(function (response) {
-                var cdata = response.data;
-                var status = response.status;
-                var statusText = response.statusText;
-                var headers = response.headers;
-                var config = response.config;
-
-                app.cities = cdata.results;
-                console.log(cdata.results);
-                console.log(status);
-                app.city = '';
+                app.cities = response.data.results;
+                app.city = "";
 
             }, function (error) {
                 console.log(error, 'can not get data.');
             });
-
     };
 
     refreshData();
 
     app.addCityState = function (city) {
         $http.post('http://0.0.0.0:8000/todo/api/v1.0/cities', {"cname": city.cname, "state": city.state}).then(function (response) {
-            console.log(".....addCity.....");
-            console.log(city.cname);
-            console.log(city.state);
 
             app.cities.push(response.data.output);
-
-            var cdata = response.data;
-            var status = response.status;
-            var statusText = response.statusText;
-            var headers = response.headers;
-            var config = response.config;
-
-            console.log(status);
-            $scope.city = cdata.output;
-            console.log(cdata.output);
-
+            $scope.city = response.data.output;
             refreshData();
 
         }, function (error) {
             console.log(error, 'can not post the city data.');
         });
-
     };
 
     app.editState = function (city) {
         $http.get('http://0.0.0.0:8000/todo/api/v1.0/cities/' + city.cname).then(function (response) {
 
-            console.log("inside editCity");
-            console.log(city.cname);
-            console.log(city.state);
-
-
-            var cdata = response.data;
-            var status = response.status;
-            var statusText = response.statusText;
-            var headers = response.headers;
-            var config = response.config;
-
-            console.log(status);
-            $scope.city = cdata.output;
+            $scope.city = response.data.output;
 
         }, function (error) {
             console.log(error, 'can not get the given city data.');
@@ -86,48 +48,25 @@ app.controller("AppCtrl", function ($scope, $http) {
 
     // updates the state for a given city
     app.updateState = function (city) {
-        if (city.cname === '' &&  city.state == '') {
-            alert("Please enter the values");
-        }else{
+        if (city.cname !== '' &&  city.state !== '') {
             $http.put('http://0.0.0.0:8000/todo/api/v1.0/cities/' + city.cname, {
                 "cname": city.cname,
                 "state": city.state
             }).then(function (response) {
 
-
-                var cdata = response.data;
-                var status = response.status;
-                var statusText = response.statusText;
-                var headers = response.headers;
-                var config = response.config;
-
-                console.log(status);
-                $scope.city = cdata.output;
-                console.log(cdata.output);
+                $scope.city = response.data.output;
                 refreshData();
 
             }, function (error) {
                 console.log(error, 'can not update given city data.');
             });
         }
-
-    }
+    };
 
     app.deleteCityState = function (city) {
         $http.delete('http://0.0.0.0:8000/todo/api/v1.0/cities/' + city.cname).then(function (response) {
 
-            console.log("inside deleteCity");
-            console.log(city.cname);
-
-            var cdata = response.data;
-            var status = response.status;
-            var statusText = response.statusText;
-            var headers = response.headers;
-            var config = response.config;
-
-            console.log(status);
-            $scope.city = cdata.output;
-            console.log(cdata.output);
+            $scope.city = response.data.output;
             refreshData();
 
         }, function (error) {
