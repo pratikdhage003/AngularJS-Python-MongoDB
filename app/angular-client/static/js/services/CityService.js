80b15b5f0c81a04d199ceae0d10c7a78bdbd9fe7
Pratik Dhage <pratikdhage@PMP.local>
2019-01-01 22:29:09 -0800

    changes in angularjs service URIs for calling the flask server

--- app/angular-client/static/js/services/CityService.js
+++ app/angular-client/static/js/services/CityService.js
@@ -10,7 +10,7 @@ var CityService = angular.module('CityService', []);
 
 CityService.factory('CityFactoryAPI', ['$http', function ($http) {
 
-    var baseURL = 'http://0.0.0.0:8001/todo/api/v1.0/cities/';
+    var baseURL = 'http://3.17.134.171:8001/todo/api/v1.0/cities/';
     var CityFactoryAPI = {};
 
     CityFactoryAPI.refreshCityStateService = function () {
